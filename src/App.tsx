8c2efd5646b4f7ee30cf81f8e0c9c34c40f6483a
1808Timur <akezhev.itrum@gmail.com>
2023-10-19 16:37:59 +0300

    [#5] change hello4 to hello5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>hello2 world</h1>
+      <h1>hello5 world</h1>
     </div>
   );
 }
-
 export default App;
